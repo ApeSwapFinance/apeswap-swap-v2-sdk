333441c96d1e6f3c4b870d740ea369bc3823c2fe
bronco <rg3mvdb0@protonmail.com>
2022-06-23 11:56:30 +0200

    adding sdk factories

---
+++ balancer-js/src/test/factories/named-tokens.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const namedTokens: Record<string, any> = {
+  wstETH: {
+    address: '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0',
+    decimals: 18,
+  },
+  wETH: {
+    address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    decimals: 18,
+  },
+  wBTC: {
+    address: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+    decimals: 8,
+  },
+};
